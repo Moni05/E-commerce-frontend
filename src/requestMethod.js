dac7bb061cb1fcfbf13d4943ecc68e3ac936ba58
Moni Shankar <32106437+Moni05@users.noreply.github.com>
2022-03-20 21:39:14 +0530

    updating token values from local storage

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001/";
+const BASE_URL = process.env.REACT_APP_URL;
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
-const TOKEN = currentUser.accessToken;
+const TOKEN = currentUser && currentUser.accessToken;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
