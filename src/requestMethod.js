1da496d664aa7ea86f0f4a268fdd1fb9d3cfc241
Moni Shankar <32106437+Moni05@users.noreply.github.com>
2022-03-21 16:29:57 +0530

    getting token value from local storage

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -5,7 +5,7 @@ const BASE_URL = process.env.REACT_APP_URL;
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
-
+console.log(TOKEN);
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
